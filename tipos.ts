2886721af441b168ce0c4c67549853e25daf3a9e
lucazaharic <lucazaharic@gmail.com>
2024-04-04 20:44:21 -0300

    adicionando o typescript

---
+++ tipos.ts
@@ -0,0 +1,9 @@
+
+let idade: number = 22;
+let nome: string = 'Pedro';
+let solteiro: boolean = true;
+let arrayNumeros: number[] = [1, 23, 45, 9, 10];
+let pessoa: { nome: string, idade: number } = {
+    nome: "Carla",
+    idade: 30
+}
